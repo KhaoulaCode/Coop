fd59b38f0de4b33a9f4ee5ba96382cc0a6ef7bf0
Emilien <emilien.ginko@gmail.com>
2023-02-01 22:56:44 +0100

    💬💄 create button component

--- src/components/atoms/Button.vue
+++ src/components/atoms/Button.vue
@@ -4,20 +4,28 @@ const props = defineProps({
   type: String,
   content: String,
   callBack: Function,
+  to: String,   
 })
 
 </script>
 <template>
 
-    <button
+    <button v-if="type!=='link'"
         :class="'coop-button coop-button-' + type"
         @click="callBack"
         :type="type===submit ? 'submit' : ''"
     >    
+        
         {{ content }}
+        
     </button>
 
-
+    <button 
+        :class="'coop-button coop-button-' + type"
+        v-if="type==='link'"
+    >
+        <RouterLink :to="to" class="bi-button-text">{{ content }}</RouterLink>
+    </button>
 
 
 </template>

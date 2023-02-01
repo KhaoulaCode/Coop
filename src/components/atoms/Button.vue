7d0953ce5adeef1527af8892350444a2a792a8e1
Emilien <emilien.ginko@gmail.com>
2023-02-01 11:59:27 +0100

    💬 Create button component

---
+++ src/components/atoms/Button.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+const props = defineProps({
+  type: String,
+  content: String,
+  callBack: Function,
+})
+
+</script>
+<template>
+
+    <button
+        :class="'coop-button coop-button-' + type"
+        @click="callBack"
+        :type="type===submit ? 'submit' : ''"
+    >    
+        {{ content }}
+    </button>
+
+
+
+
+</template>

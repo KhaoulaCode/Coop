4358a6ac8ed58e3eb1bcc9d60a257d5978e52d0f
Emilien <emilien.ginko@gmail.com>
2023-02-01 22:58:15 +0100

    💬💄 create label component

---
+++ src/components/molecules/Label.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import "../../assets/styles/components/molecules/label.scss"
+    import Tag from "../atoms/Tag.vue"
+    const props = defineProps({
+        channel: Object,
+    })
+</script>
+
+<template>
+    <router-link :to="{name: 'conversation', params: {id :channel.id}}">
+        <div class="coop-label">
+
+            <h2>{{channel.label}}</h2>
+            <Tag :text="channel.topic"/>
+        </div>    
+    </router-link>
+</template>

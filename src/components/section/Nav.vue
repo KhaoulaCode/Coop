2d156ab0f952f0da08ea0f983bd4f86a37f5d458
Emilien <emilien.ginko@gmail.com>
2023-02-02 14:42:46 +0100

    💬 disconnect in nav

--- src/components/section/Nav.vue
+++ src/components/section/Nav.vue
@@ -1,5 +1,12 @@
-<script>
+<script setup>
 import "../../assets/styles/components/section/nav.scss"
+import Button from "../atoms/Button.vue"
+import { useSessionStore } from '@/stores/session';
+const session = useSessionStore();
+
+const logout = ()=>{
+  session.logout()
+}
 </script>
 
 <template>
@@ -9,5 +16,8 @@ import "../../assets/styles/components/section/nav.scss"
     <RouterLink to="/creer-conversation">creer conversation</RouterLink> |
     <RouterLink to="/login">Login</RouterLink> |
     <RouterLink to="/register">Créer un compte</RouterLink>
+    <Button v-if="session.isValid()" type="text" content="Se déconnecter" :callBack="logout"/> 
+    <Button type="link" content="Se connecter" to="/login"/> 
+    <Button type="link" content="Créer un compte" to="/register"/> 
   </nav>
 </template>
